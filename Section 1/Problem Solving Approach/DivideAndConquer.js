024417d4f0729cd7123a71cf1b6960ff8527fd81
payalpatra <payalpatra105@gmail.com>
2021-06-23 20:43:28 +0530

    Changed Folder Structure

--- Section 1/Problem Solving Approach/DivideAndConquer.js
+++ Section 1/Problem Solving Approach/DivideAndConquer.js
@@ -28,3 +28,26 @@ function Search2(arr, num) {
 
 console.log(Search2([1, 2, 3, 4, 5, 6, 7], 4))
 
+function divideAndConquer(arr, num) {
+
+    let min = 0;
+    let max = arr.length - 1;
+
+    while (min <= max) {
+
+        // Divide the array until we satisfy one of the condition
+        let mid = Math.floor((min + max) / 2)
+
+        if (arr[mid] < num) {
+            min = mid + 1;
+        } else if (arr[mid] > num) {
+            max = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+
+    return -1
+}
+
+console.log(divideAndConquer([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 18], 5))
