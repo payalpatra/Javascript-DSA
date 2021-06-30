15f9a75d2b2c3981719eaafc52c76ad5cc8a34fd
payalpatra <payalpatra105@gmail.com>
2021-06-30 23:45:22 +0530

    Added Singly Linked List Push

---
+++ Data Structures/Linked Lists/Singly Linked Lists/1_Intoduction.js
@@ -0,0 +1,65 @@
+/*
+** What is a Linked List ?
+-- A data Structure that contains a head, tail and length property.
+-- Linked Lists consist of nodes, and each node has a value and a pointer to another node o null.
+ 
+* Singly Linked List *
+
+Head  Length = 4  Tail
+ _____________________
+|                     |
+|                     |
+[4]--->[6]--->[8]--->[2]---> 
+   next   next   next   null
+
+
+Comparison with arrays
+-----------------------
+
+LISTS --
+
+- Do not have Indexes 
+- Connected via nodes with a next Pointer.
+- Random access is not allowed.
+- Good at Insertion and Deletion.
+
+ARRAYS --
+
+- Indexed in Order 
+- Insertion and deletion can be expensive
+- Can Quickly be accessed at a specific Index.
+*/
+
+
+// Piece of Data --- val
+// Reference to next node - next
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+    static getLists(...nodes) {
+        return nodes
+    }
+}
+
+/// Bad representation of adding new Nodes 
+
+let first = new Node(20)
+first.next = new Node(30);
+first.next.next = new Node(40)
+
+
+// console.log(Node.getLists(first, first.next, first.next.next))
+// Output
+
+/*
+[
+    Node { val: 'Hi', next: Node { val: 'There', next: [Node] } },
+    Node { val: 'There', next: Node { val: 'Payal', next: null } },
+    Node { val: 'Payal', next: null }
+]
+*/
+
+
---
+++ Data Structures/Linked Lists/Singly Linked Lists/2_Push.js
@@ -0,0 +1,81 @@
+// PUSHING - Adding a new node to the end of the Linked List.
+
+/// ------ PUSHING PSEUDOCODE ------ \\\
+
+// -- This function should accept a value. 
+// -- Create a new node using the value passed to the function.
+// -- If there is no head property on the list, set the head and the tail to the newly created node.
+// -- Otherwise set the next property on the list to be the new node and set the tail property on the list to be the newly created node.
+// -- Increament the length by one.
+// -- Return the Linked List
+
+/// Node -- Each Node will have a value and and next is the pointer to another node.
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+
+}
+
+/// Singly Linked List -- This will have head , tail , length and all the nodes.
+
+class singlyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    // Instance Method
+    push(val) {
+
+        // Instance of Node class
+        let newNode = new Node(val);
+
+        // If the list is empty
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode
+        }
+        this.length++;
+        return this
+    }
+}
+
+
+// Instance of singlyLinkedList class
+let list = new singlyLinkedList()
+
+list.push(20)
+
+// Output -- Pushes 20 into the Linked List and Increaments 1
+
+/// List after pushing 20
+/** 
+{
+  head: Node { val: 20, next: null },
+  tail: Node { val: 20, next: null },
+  length: 1
+}
+**/
+
+list.push(30)
+
+// Output -- Pushes 30 into the Linked List and Increaments
+
+/// List after pushing 30
+
+/**
+{
+    head: Node { val: 20, next: Node { val: 30, next: null } },
+    tail: Node { val: 30, next: null },
+    length: 2
+}
+**/
+
+console.log(list)
